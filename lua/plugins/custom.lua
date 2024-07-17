375d975ebae32cef6e5ec39e868b7d474127d762
ilhamSuandi <ilham15suandi@gmail.com>
2024-07-17 17:46:29 +0700

    feat: add lazygit into nvim

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -122,10 +122,69 @@ return {
   },
 
   {
+    -- NOTE : nvim-colorizer for coloring hex code / colors
     "norcalli/nvim-colorizer.lua",
     lazy = false,
     config = function()
       require("colorizer").setup()
     end,
+  },
+
+  {
+    -- NOTE: LazyGit integration
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.g.lazygit_floating_window_scaling_factor = 0.5 -- scaling factor for floating window
+      vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window
+
+      -- Create the floating window
+      local function open_lazygit()
+        local width = math.floor(vim.o.columns * 0.8)
+        local height = math.floor(vim.o.lines * 0.8)
+        local opts = {
+          relative = "editor",
+          width = width,
+          height = height,
+          col = math.floor((vim.o.columns - width) / 2),
+          row = math.floor((vim.o.lines - height) / 2),
+          border = "rounded",
+          style = "minimal",
+        }
+
+        local buf = vim.api.nvim_create_buf(false, true)
+        local win = vim.api.nvim_open_win(buf, true, opts)
+        vim.wo[win].winblend = 0
+        vim.wo[win].wrap = false
+        vim.wo[win].scrolloff = 0
+        vim.wo[win].sidescrolloff = 8 -- Allow some margin for scrolling
+
+        vim.fn.termopen("lazygit", {
+          on_exit = function(_, _, _)
+            vim.api.nvim_win_close(win, true)
+          end,
+        })
+
+        -- Switch to insert mode with an autocommand
+        -- vim.cmd "startinsert"
+      end
+
+      vim.api.nvim_create_user_command("LazyGit", open_lazygit, {})
+    end,
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
   },
 }
